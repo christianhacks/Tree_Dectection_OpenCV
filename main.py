deed240389d7d7a975721fef000bb023c776dcc0
Christian <christian2.ekeigwe@gmail.com>
2021-08-03 20:07:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,22 @@ import time
 import cv2
 import numpy as np
 from os.path import isfile, join
-import matplotlib.pyplot as plt
 
-tree_classifier = cv2.CascadeClassifier('/Users/christianekeigwe/Desktop/Desktop/Purdue/Summer 2021/CNIT 39000/treedetection/cascade.xml')
+tree_classifier = cv2.CascadeClassifier('<Cascade_File_Path>')
 
-cap = cv2.VideoCapture('/Users/christianekeigwe/Desktop/Desktop/Purdue/Summer 2021/CNIT 39000/treedetection/DJI_0014.MP4')
+cap = cv2.VideoCapture('<Video_File_Path>')
 
-#"""
-tree_classifier = cv2.CascadeClassifier('/Users/christianekeigwe/Desktop/Desktop/Purdue/Summer 2021/CNIT 39000/treedetection/cascade.xml')
-
-cap = cv2.VideoCapture('/Users/christianekeigwe/Desktop/Desktop/Purdue/Summer 2021/CNIT 39000/treedetection/DJI_0014.MP4')
-
-while 1:
+while True:
     time.sleep(.05)
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     trees = tree_classifier.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in trees:
         image = cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), 2)
-
-    cv2.imshow('Trees', image)
-    cv2.waitKey()
+        cv2.imshow('Trees', image)
+        cv2.namedWindow('Trees', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Trees', 1900, 1000)
+        cv2.waitKey(1)
 
 cap.release()
-#cv2.destroyAllWindows()
-#"""
+cv2.destroyAllWindows()
